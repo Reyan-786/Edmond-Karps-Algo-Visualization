520f4542675e503fa02e3f43302b6645ab39bd72
Mohd Rehan <reyanabidin169@gmail.com>
2023-09-06 00:39:20 +0530

    Updated Visualizer

--- graph.js
+++ graph.js
@@ -1,28 +1,27 @@
 const svg = d3.select("#graph-svg");
 
-
-    
 const graph = {
     "nodes": [
-        {id: 0, name: 's', fixed: true, x: 147, y: 243},
-        {id: 1, name: 't', fixed: true, x: 764, y: 254}, 
-        {id: 2, name: 'v1',fixed: true, x: 346, y: 118},
-        {id: 3, name: 'v2',fixed: true, x: 380, y: 300},
-        {id: 4, name: 'v3',fixed: true, x: 545, y: 136},
-        {id: 5, name: 'v4',fixed: true, x: 599, y: 309}
+        { id: 0, name: 's', fixed: true, x: 147, y: 243 },
+        { id: 1, name: 't', fixed: true, x: 764, y: 254 },
+        { id: 2, name: 'v1', fixed: true, x: 346, y: 118 },
+        { id: 3, name: 'v2', fixed: true, x: 380, y: 300 },
+        { id: 4, name: 'v3', fixed: true, x: 545, y: 136 },
+        { id: 5, name: 'v4', fixed: true, x: 599, y: 309 }
     ],
-     "links": [
-        {source: 0, target: 2, capacity: 16,flow: 0, residualCapacity:16},
-        {source: 2, target: 4, capacity: 12,flow: 0, residualCapacity:12},
-        {source: 4, target: 1, capacity: 20,flow: 0, residualCapacity:20},
-        {source: 0, target: 3, capacity: 13,flow: 0, residualCapacity:13},
-        {source: 3, target: 5, capacity: 14,flow: 0, residualCapacity:14},
-        {source: 5, target: 1, capacity: 4,flow: 0, residualCapacity:4},
-        {source: 3, target: 2, capacity: 4,flow: 0, residualCapacity:4},
-        {source: 4, target: 3, capacity: 9,flow: 0, residualCapacity:9},
-        {source: 5, target: 4, capacity: 7,flow: 0, residualCapacity:7}
-      ]
+    "links": [
+        { source: 0, target: 2, capacity: 16, flow: 0, residualCapacity: 16 },
+        { source: 2, target: 4, capacity: 12, flow: 0, residualCapacity: 12 },
+        { source: 4, target: 1, capacity: 20, flow: 0, residualCapacity: 20 },
+        { source: 0, target: 3, capacity: 13, flow: 0, residualCapacity: 13 },
+        { source: 3, target: 5, capacity: 14, flow: 0, residualCapacity: 14 },
+        { source: 5, target: 1, capacity: 4, flow: 0, residualCapacity: 4 },
+        { source: 3, target: 2, capacity: 4, flow: 0, residualCapacity: 4 },
+        { source: 4, target: 3, capacity: 9, flow: 0, residualCapacity: 9 },
+        { source: 5, target: 4, capacity: 7, flow: 0, residualCapacity: 7 }
+    ]
 }
+
 const totalNodeDiv = document.getElementById("displayTotalNodes");
 const arrowMarker = svg.append("defs")
     .append("marker")
@@ -43,39 +42,34 @@ totalNodeDiv.style.fontSize = "30px";
 totalNodeDiv.style.textAlign = 'center';
 
 const simulation = d3.forceSimulation(graph.nodes)
-    .force("charge",d3.forceManyBody().strength(-200))
-    .force("link",d3.forceLink(graph.links).distance(250))
+    .force("charge", d3.forceManyBody().strength(-200))
+    .force("link", d3.forceLink(graph.links).distance(250))
     .force("center", d3.forceCenter(svg.attr("width") / 2, svg.attr("height") / 2));
 
-    const links = svg.selectAll(".link")
+const links = svg.selectAll(".link")
     .data(graph.links)
     .enter().append("line")
     .attr("class", "link")
     .attr("stroke", "red")
-    .attr("stroke-width", 2)
-    // .attr("marker-end", "url(#arrow-marker)")
-    // .attr("x1", d => graph.nodes[d.source].x)
-    // .attr("y1", d => graph.nodes[d.source].y)
-    // .attr("x2", d => graph.nodes[d.target].x)
-    // .attr("y2", d => graph.nodes[d.target].y);
+    .attr("stroke-width", 2);
 
-const linkFlowbyCapacity = svg.selectAll(".link-label")
-        .data(graph.links)
-        .enter()
-        .append("text")
-        .attr("class", "link-label")
-        .attr("dy", -15) 
-        .attr("font-size",'20px')
-        .style("fill", "black")
-        .text(d => `${d.flow}/${d.capacity}`);
+    const linkFlowbyCapacity = svg.selectAll(".link-label")
+    .data(graph.links)
+    .enter()
+    .append("text")
+    .attr("class", d => `link-label link-label-${d.source}-${d.target}`)
+    .attr("dy", -15)
+    .attr("font-size", '20px')
+    .style("fill", "black")
+    .text(d => `${d.flow}/${d.capacity}`);
+
 
 const nodes = svg.selectAll(".node")
     .data(graph.nodes)
     .enter().append("circle")
     .attr("class", "node")
     .attr("r", 20)
-    .attr("fill", "#0073e6")
-    .call(drag(simulation));
+    .attr("fill", "#0073e6");
 
 const labels = svg.selectAll(".label")
     .data(graph.nodes)
@@ -84,38 +78,62 @@ const labels = svg.selectAll(".label")
     .attr("text-anchor", "middle")
     .attr("fill", "black")
     .attr("font-size", "20px")
-    .each(function(d) {
+    .each(function (d) {
         d3.select(this).append("tspan")
-            .attr("dy", 5) 
-            .text(d.name);
+            .attr("dy", 5)
+            .text(d.id);
     });
 
-    let moveEnabled = false;
-    let dragBehavior = null;
+let moveEnabled = false;
+let dragBehavior = null;
 
-    const toggleMoveButton = d3.select("#toggle-move");
+const toggleMoveButton = d3.select("#toggle-move");
 
-    toggleMoveButton.on('click', function() {
-        moveEnabled = !moveEnabled;
-    
-        if (moveEnabled) {
-            dragBehavior = drag(simulation);
-            nodes.call(dragBehavior);
-        } else {
+toggleMoveButton.on('click', function () {
+    moveEnabled = !moveEnabled;
+    if (moveEnabled) {
+        dragBehavior = d3.drag()
+            .on("start", dragStarted)
+            .on("drag", dragged)
+            .on("end", dragEnded);
+        nodes.call(dragBehavior);
+        toggleMoveButton.text("Disable Move");
+    } else {
+        if (dragBehavior) {
             nodes.on(".drag", null);
-            dragBehavior = null;
+            toggleMoveButton.text("Enable Move");
         }
-    
-        toggleMoveButton.text(moveEnabled ? "Move Enabled" : "Move Disabled");
-    });
+    }
+});
+
+function dragStarted(event) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
+}
+
+function dragged(event) {
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
+}
+
+function dragEnded(event) {
+    if (!event.active) simulation.alphaTarget(0);
+    event.subject.fx = null;
+    event.subject.fy = null;
+}
 
 simulation.on("tick", () => {
-    
     links
         .attr("x1", d => d.source.x)
         .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x) 
+        .attr("x2", d => d.target.x)
         .attr("y2", d => d.target.y);
+
+    linkFlowbyCapacity
+        .attr("x", d => (d.source.x + d.target.x) / 2)
+        .attr("y", d => (d.source.y + d.target.y) / 2)
+        .text(d => `${d.flow}/${d.capacity}`);
 
     nodes
         .attr("cx", d => d.x)
@@ -124,55 +142,7 @@ simulation.on("tick", () => {
     labels
         .attr("x", d => d.x)
         .attr("y", d => d.y);
-
-    linkFlowbyCapacity
-        .attr("x", d => (d.source.x + d.target.x) / 2)
-        .attr("y", d => (d.source.y + d.target.y) / 2);
 });
-
-function drag(simulation) {
-    function dragStarted(event) {
-        if (!event.active) simulation.alphaTarget(0.3).restart();
-        event.subject.fx = event.subject.x;
-        event.subject.fy = event.subject.y;
-    }
-    function dragged(event) {
-        event.subject.fx = event.x;
-        event.subject.fy = event.y;
-    }
-    function dragEnded(event) {
-        if (!event.active) simulation.alphaTarget(0);
-        event.subject.fx = event.x;
-        event.subject.fy = event.y;
-    }
-
-    return d3.drag()
-        .on("start", dragStarted)
-        .on("drag", dragged)
-        .on("end", dragEnded);
-}
-let PrimaryGraph = {
-    "nodes": [
-        {id: 0, name: 's', fixed: true, x: 147, y: 243},
-        {id: 1, name: 't', fixed: true, x: 764, y: 254}, 
-        {id: 2, name: 'v1',fixed: true, x: 346, y: 118},
-        {id: 3, name: 'v2',fixed: true, x: 380, y: 300},
-        {id: 4, name: 'v3',fixed: true, x: 545, y: 136},
-        {id: 5, name: 'v4',fixed: true, x: 599, y: 309}
-    ],
-     "links": [
-        {source: 0, target: 2, capacity: 16,flow: 0, residualCapacity:16},
-        {source: 2, target: 4, capacity: 12,flow: 0, residualCapacity:12},
-        {source: 4, target: 1, capacity: 20,flow: 0, residualCapacity:20},
-        {source: 0, target: 3, capacity: 13,flow: 0, residualCapacity:13},
-        {source: 3, target: 5, capacity: 14,flow: 0, residualCapacity:14},
-        {source: 5, target: 1, capacity: 4,flow: 0, residualCapacity:4},
-        {source: 3, target: 2, capacity: 4,flow: 0, residualCapacity:4},
-        {source: 4, target: 3, capacity: 9,flow: 0, residualCapacity:9},
-        {source: 5, target: 4, capacity: 7,flow: 0, residualCapacity:7}
-      ]
-}
-
 function bfs(source, target, graph) {
     const queue = [source];
     const visited = new Set();
@@ -216,40 +186,12 @@ function reconstructPath(path, source, target) {
     result.unshift(source);
     return result;
 }
-function updateEdgeCapacity(sourceNode, targetNode, newCapacity) {
-    const edgeLabel = svg.select(`.link-label[data-source="${sourceNode}"][data-target="${targetNode}"]`);
-    edgeLabel.text(newCapacity);
-}
-// function updateCapacities(graph, path, minCapacity) {
-//     for (let i = 1; i < path.length; i++) {
-//         const currentNode = path[i - 1];
-//         const nextNode = path[i];
-
-//         const edgesOfCurrentNode = graph.nodes[currentNode].edges; // Change this line
-
-//         for (const edge of edgesOfCurrentNode) { // Use the correct edges array
-//             if (edge.target === nextNode) {
-//                 edge.residualCapacity -= minCapacity;
-
-//                 // If there's a corresponding reverse edge, update it
-//                 const reverseEdge = graph.nodes[nextNode].edges.find(e => e.target === currentNode); // Change this line
-//                 if (reverseEdge) {
-//                     reverseEdge.residualCapacity += minCapacity;
-//                 }
-
-//                 // Update the edge visualization
-//                 updateEdgeCapacity(currentNode, nextNode, edge.residualCapacity);
-//                 break;
-//             }
-//         }
-//     }
-// }
 
 function edmondkarp(source, target, graph) {
 
     const residualGraph = JSON.parse(JSON.stringify(graph));
     let maxflow = 0;
-    let minCapacity = Infinity;
+    const paths_info = new Set();
     while (true) {
         const augmentedPath = bfs(source, target, residualGraph);
         console.log(augmentedPath);
@@ -257,6 +199,7 @@ function edmondkarp(source, target, graph) {
             break;
         }
         
+        let minCapacity = Infinity;
         
         for (let i = 0; i < augmentedPath.length - 1; i++) {
             const link = findLink(augmentedPath[i], augmentedPath[i + 1], residualGraph);
@@ -265,38 +208,21 @@ function edmondkarp(source, target, graph) {
         console.log("min capacity : " , minCapacity);
 
         maxflow += minCapacity;
+        paths_info.add({ path: augmentedPath, capacity: minCapacity });
 
-        // updateCapacities(graph,augmentedPath,minCapacity);
         for (let i = 0; i < augmentedPath.length - 1; i++) {
             updateFlow(augmentedPath[i], augmentedPath[i + 1], minCapacity, graph);
             updateFlow(augmentedPath[i], augmentedPath[i + 1], minCapacity, residualGraph);
 
         }
     }
-    return maxflow;
+
+    // Convert Set back to an array
+    const uniquePathsInfo = [...paths_info];
+
+    return { paths: uniquePathsInfo, maxflow };
 
 }
-function findLink(source,target,graph ) {
-    return graph.links.find(link => link.source === source && link.target === target );
-}
-
-function updateGraphDisplay() {
-    alert("Now updating the graph with the modified Flow values!!");
-    // const links = svg.selectAll(".link"); // Select the links within the current graph state
-
-    // links.attr("x1", d => d.source.x)
-    //      .attr("y1", d => d.source.y)
-    //      .attr("x2", d => d.target.x)
-    //      .attr("y2", d => d.target.y);
-
-    const linkFlowbyCapacity = svg.selectAll(".link-label"); // Select the link labels within the current graph state
-    linkFlowbyCapacity.attr("x", d => (d.source.x + d.target.x) / 2)
-                     .attr("y", d => (d.source.y + d.target.y) / 2)
-                     .text(d => `${d.flow}/${d.capacity}`);
-    alert("Updated the flow values ");
-
-}
-
 
 
 function updateFlow(source, target, amount, graph) {
@@ -325,14 +251,103 @@ function updateFlow(source, target, amount, graph) {
     // Rest of your code
 }
 
+let primary_Graph = {
+    "nodes": [
+        { id: 0, name: 's', fixed: true, x: 147, y: 243 },
+        { id: 1, name: 't', fixed: true, x: 764, y: 254 },
+        { id: 2, name: 'v1', fixed: true, x: 346, y: 118 },
+        { id: 3, name: 'v2', fixed: true, x: 380, y: 300 },
+        { id: 4, name: 'v3', fixed: true, x: 545, y: 136 },
+        { id: 5, name: 'v4', fixed: true, x: 599, y: 309 }
+    ],
+    "links": [
+        { source: 0, target: 2, capacity: 16, flow: 0, residualCapacity: 16 },
+        { source: 2, target: 4, capacity: 12, flow: 0, residualCapacity: 12 },
+        { source: 4, target: 1, capacity: 20, flow: 0, residualCapacity: 20 },
+        { source: 0, target: 3, capacity: 13, flow: 0, residualCapacity: 13 },
+        { source: 3, target: 5, capacity: 14, flow: 0, residualCapacity: 14 },
+        { source: 5, target: 1, capacity: 4, flow: 0, residualCapacity: 4 },
+        { source: 3, target: 2, capacity: 4, flow: 0, residualCapacity: 4 },
+        { source: 4, target: 3, capacity: 9, flow: 0, residualCapacity: 9 },
+        { source: 5, target: 4, capacity: 7, flow: 0, residualCapacity: 7 }
+    ]
+}
+function findLink(source, target, GRAPH) {
+    for (const link of GRAPH.links) {
+        if(link.source === source && link.target === target) {
+            return link;
+        }
+    }
+    return null;
+}
+
+const runButton = document.getElementById("Run-Algo");
+runButton.addEventListener('click', function() {
+    const sourceNode = parseInt(prompt("Enter the Source Node : "));
+    const TargetNode = parseInt(prompt("Enter the Target Node : "));
+
+    const { paths, maxflow } = edmondkarp(sourceNode, TargetNode, primary_Graph);
+
+    const showPathButton = document.getElementById('highlight-Next-Path');
+    let currentPathIndex = 0;
+    simulation.tick();
+    showPathButton.addEventListener('click', function() {
+        if (currentPathIndex < paths.length) {
+            animatePath(paths[currentPathIndex].path, paths[currentPathIndex].capacity);
+            currentPathIndex++;
+            
+        } else {
+            
+            console.log("All paths have been highlighted.");
+        }
+    });
+});
+
+// function updateGraphDisplay(path, capacity) {
+//     alert("Now updating the graph with the modified Flow values!!");
+//     // const links = svg.selectAll(".link"); // Select the links within the current graph state
+
+//     // links.attr("x1", d => d.source.x)
+//     //      .attr("y1", d => d.source.y)
+//     //      .attr("x2", d => d.target.x)
+//     //      .attr("y2", d => d.target.y);
+
+
+//     const linkFlowbyCapacity = svg.selectAll(".link-label"); // Select the link labels within the current graph state
+//     linkFlowbyCapacity.attr("x", d => (d.source.x + d.target.x) / 2)
+//                      .attr("y", d => (d.source.y + d.target.y) / 2)
+//                      .text(d => `${d.flow+capacity}/${d.capacity}`);
+//     alert("Updated the flow values ");
+
+// }
+
+function updateGraphDisplay(path, capacity) {
+
+    for(let i = 1; i< path.length; i++) {
+        const currentNode = path[i-1];
+        const nextNode = path[i];
+        const linkFlowbyCapacity = svg.selectAll(".link-label")
+        .filter(function(d) {
+            // Use the filter function to check if the data matches the source and target nodes
+            return d.source.id === currentNode && d.target.id === nextNode;
+        });
+    
+        linkFlowbyCapacity.attr("x", d => (d.source.x + d.target.x) / 2)
+                         .attr("y", d => (d.source.y + d.target.y) / 2)
+                         .text(d => `${d.flow+capacity}/${d.capacity}`);
+    
+    }
+    
+
+}
 
 
 const animationDelay = 2000;
-function animatePath(path) {
+function animatePath(path,capacity) {
     let index = 0;
 
     function highlightStep() {
-        if (index < path.length) {
+        if (index < path.length-1) {
             const currentNode = path[index];
             highlightNode(currentNode);
 
@@ -346,7 +361,7 @@ function animatePath(path) {
         } else {
             setTimeout(() => {
                 unhighlightPath(path);
-                updateGraphDisplay();
+                updateGraphDisplay(path, capacity);
             }, animationDelay);
         }
     }
@@ -380,18 +395,21 @@ function unhighlightPath(path) {
         .style("stroke", "red"); 
 }
 
-const runButton = document.getElementById("Run-Algo");
-runButton.addEventListener('click', function() {
-    const sourceNode = parseInt(prompt("Enter the Source Node : "));
-    const TargetNode = parseInt(prompt("Enter the Target Node : "));
+// function highlightPath(path, capacity) {
+//     for (let i = 0; i < path.length - 1; i++) {
+//         const source = path[i];
+//         const target = path[i + 1];
+//         const graphLink = findLink(source, target, primary_Graph);
 
-    const path = bfs(sourceNode,TargetNode,PrimaryGraph);
-    if(path) {
-        const maxFlow = edmondkarp(sourceNode,TargetNode,PrimaryGraph);
-        animatePath(path);
-        alert("Maximum Flow : "+maxFlow);
-    }
-    else {
-        alert("No path found!");
-    }
-});
+//         if (graphLink) {
+//             const currentFlow = graphLink.flow;
+//             const capacityLink = graphLink.capacity;
+//             const newFlow = currentFlow + capacity;
+//             graphLink.flow = newFlow;
+
+//             // Select the corresponding link label using a unique class
+//             svg.selectAll(`.link-label-${source}-${target}`)
+//                 .text(`${newFlow}/${capacityLink}`);
+//         }
+//     }
+// }
